d46c690f4d97b799a5ae2a2d3fc3524eb2d2bd29
DESKTOP-P4US2OO\DELL <yangcheng@wyebus.com>
2019-11-19 17:43:05 +0800

    Site updated: 2019-11-19 17:42:59

---
+++ js/src/test.js
@@ -0,0 +1,17 @@
+
+console.log(1231231)
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', function () {
+      navigator.serviceWorker.register('/sw.js', {scope: '/'})
+          .then(function (registration) {
+              console.log(1111)
+              // 注册成功
+              console.log('ServiceWorker registration successful with scope: ', registration.scope);
+          })
+          .catch(function (err) {
+              console.log(2222)
+              // 注册失败:(
+              console.log('ServiceWorker registration failed: ', err);
+          });
+  });
+}
